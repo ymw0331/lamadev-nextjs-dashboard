c69a0998a6d1f3485fe4b2816109ab0f166ac035
Wayne Yong <ymw0331@gmail.com>
2024-12-30 00:23:39 +0800

    feat: recharts of count, attendance and finance

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,7 @@
 import { UserCard } from "@/components/UserCard"
+import CountChart from "@/components/CountChart"
+import AttendanceChart from "@/components/AttendanceChart"
+import FinanceChart from "@/components/FinanceChart"
 
 const AdminPage = () => {
     return (
@@ -6,7 +9,8 @@ const AdminPage = () => {
             className="p-4 flex gap-4 flex-col md:flex-row">
 
             {/* LEFT */}
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-2/3 flex flex-col gap8">
+
                 {/* USER CARDS */}
                 <div className="flex gap-4 justify-between flex-wrap">
                     <UserCard type="student" />
@@ -14,11 +18,30 @@ const AdminPage = () => {
                     <UserCard type="parent" />
                     <UserCard type="staff" />
                 </div>
+
+                {/* MIDDLE CHART */}
+                <div className="flex gap-4 flex-col lg:flex-row">
+
+                    {/* COUNT CHART */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+
+                    {/* ATTENDANCE CHART */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart />
+                    </div>
+
+                </div>
+                {/* BOTTOM CHART */}
+                <div className="w-full h-[500px]">
+                    <FinanceChart />
+                </div>
+
             </div>
 
-
             {/* RIGHT  */}
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-1/3">
                 right hand side
             </div>
 
